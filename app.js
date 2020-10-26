feb03122403b587aa07499f9a88ef4df45db93d6
EdReyLu <edreylu10@gmail.com>
2020-10-26 08:39:41 -0600

    primer commit sin bd mongo

---
+++ app.js
@@ -0,0 +1,12 @@
+import usersRoutes from './routes/users.js';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use('/users', usersRoutes);
+
+app.get('/', (req, res) => res.send('hello from Homepage!!!'));
+app.listen(PORT, () => console.log(`server running on port: http://localhost:${PORT}`));
