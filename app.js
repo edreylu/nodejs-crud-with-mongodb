899861d59a585f70011b8262edfcd5632b589e6d
Edward Reyes Lugo <edreylu10@gmail.com>
2020-10-26 13:33:35 -0600

    se agrego conexion a mongodb

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import usersRoutes from './routes/users.js';
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import mongoose from 'mongoose';
+require('dotenv/config');
 const app = express();
 const PORT = 5000;
 
@@ -9,4 +10,12 @@ app.use(bodyParser.json());
 app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => res.send('hello from Homepage!!!'));
+
+//connection to MongoDB
+mongoose.connect(process.env.DB_CONNECTION, 
+{useNewUrlPerser : true} ,() => console.log('Connected to mongodb!!')
+);
+
 app.listen(PORT, () => console.log(`server running on port: http://localhost:${PORT}`));
+
+
